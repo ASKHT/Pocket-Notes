5daae3eef0c29e46dffe48a0252c82b11f076de5
Akshit Sharma <akshits349@gmail.com>
2024-02-11 00:16:29 +0530

    notes icons coming

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import SecureBanner from "./components/SecureBanner/SecureBanner";
 import Groupcomp from "./components/Group/Groupcomp";
 import Modal from "./components/GroupModal/Modal";
+import UserContextProvider from "./components/context/UserContextProvider";
 function App() {
   const [ismodalopen, setisemodalopen] = useState(false);
+
+  // Callback function to update parent state
 
   const open = () => {
     setisemodalopen(true);
@@ -13,13 +16,18 @@ function App() {
   };
 
   return (
-    <>
+    <UserContextProvider>
       <div className="maindiv" style={{ display: "flex" }}>
         <Groupcomp open={open} />
         <SecureBanner />
-        {ismodalopen && <Modal close={close} />}
+        {ismodalopen && (
+          <Modal
+            close={close}
+            setisemodalopensetisemodalopen={setisemodalopen}
+          />
+        )}
       </div>
-    </>
+    </UserContextProvider>
   );
 }
 
